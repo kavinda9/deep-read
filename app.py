298c09983e7bd7779d6cb1a69ab4245a1ee7584b
Kavinda Hasaranga <99938424+kavinda9@users.noreply.github.com>
2025-10-08 09:35:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,4 +157,7 @@ def cleanup(session_id):
         return jsonify({'success': False}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
